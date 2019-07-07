c1f71eaec1f7256798e3369c4781fcf33ae9c688
muratsaricomtr <ms@muratsari.com.tr>
2019-07-07 20:08:53 +0300

    Route Definitions

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 loginController = require("./controllers/loginController");
+albumController = require("./controllers/albumController");
 
 exports.appRoute = router => {
-    router.get("/login", loginController.getLoginController);
+    router.get("/login", loginController.getIndex);
+    router.post("/login", loginController.postIndex);
+    router.get("/myalbums", albumController.getIndex);
 };
