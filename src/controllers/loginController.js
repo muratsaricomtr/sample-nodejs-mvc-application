d4a18866dc8c125806787dedb1a8ee97ea9265d9
muratsaricomtr <ms@muratsari.com.tr>
2019-07-07 20:08:15 +0300

    Login Controller & View

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,3 +1,10 @@
-exports.getLoginController = (req, res, next) => {
+exports.getIndex = (req, res, next) => {
     res.render("login");
+};
+
+exports.postIndex = (req, res, next) => {
+
+    console.log(req.body.email);
+    res.writeHead(301, { Location: '/login' });
+    res.end();
 };
